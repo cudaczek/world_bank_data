ee1ae91a739981afdb4f847b81df1bde98365117
Magdalena Cudak <magdalena.cudak@gmail.com>
2020-03-29 19:58:55 +0200

    log scale and labels

--- hierarchical-example.py
+++ hierarchical-example.py
@@ -11,7 +11,8 @@ from scipy.spatial.distance import pdist
 import numpy as np
 
 all_countries = WorldBankDataLoader().all_countries()
-europe_and_central_asia_countries = {country['name']:None for country in all_countries if country['region']['id'] == 'ECS'}
+europe_and_central_asia_countries = {country['name']: None for country in all_countries if
+                                     country['region']['id'] == 'ECS'}
 for country_name in europe_and_central_asia_countries:
     country_data_path = os.path.join("demography", "downloaded_countries", country_name + ".csv")
     europe_and_central_asia_countries[country_name] = pandas.read_csv(country_data_path)
@@ -21,15 +22,15 @@ for country_name in europe_and_central_asia_countries:
 NULL_PLACEHOLDER = -1
 imputer = impute.SimpleImputer(missing_values=NULL_PLACEHOLDER, strategy='mean')
 
-
 if __name__ == "__main__":
     data_per_country = {}
     for country_name in europe_and_central_asia_countries:
         data = europe_and_central_asia_countries[country_name][
-                ['Population density (people per sq. km of land area)','Urban population (% of total population)']].fillna(NULL_PLACEHOLDER)
+            ['Population density (people per sq. km of land area)', 'Urban population (% of total population)']].fillna(
+            NULL_PLACEHOLDER)
         data_per_country[country_name] = imputer.fit_transform(data)
 
-    featured_countries = [country for (country, data) in data_per_country.items() if data.shape==(60,2)]
+    featured_countries = [country for (country, data) in data_per_country.items() if data.shape == (60, 2)]
 
     X = np.array([np.array(data_per_country[country]).flatten() for country in featured_countries])
 
@@ -38,10 +39,12 @@ if __name__ == "__main__":
     plt.title('Hierarchical Clustering Dendrogram')
     plt.xlabel('sample index')
     plt.ylabel('distance')
+    plt.yscale("log")
     dendrogram(
         Z,
         leaf_rotation=90.,  # rotates the x axis labels
         leaf_font_size=8,  # font size for the x axis labels
+        labels=np.array(featured_countries)
     )
     plt.savefig("hierarchical.png")
     plt.show()
