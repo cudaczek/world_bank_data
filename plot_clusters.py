c56b7c8d96b649d0ee2b2ad688be0baace6e832a
Aleksandra Drewnik <aleksandra.drewnik@gmail.com>
2020-03-29 16:51:50 +0200

    Cluster and plot using all demographic indicators

---
+++ plot_clusters.py
@@ -0,0 +1,47 @@
+import numpy as np
+from matplotlib import pyplot as plt, colors, cm
+from sklearn import decomposition
+
+
+def plot_with_pca(X, assigned_cluster_numbers, point_labels, features_count, coeff_labels):
+    pca = decomposition.PCA(n_components=2)
+    principal_components = pca.fit_transform(X)
+    scatter_points(assigned_cluster_numbers, point_labels, principal_components)
+    plot_coeffients(coeff_labels, features_count, pca)
+    plt.show()
+
+    print_highest_coefficents(coeff_labels, pca)
+
+
+def print_highest_coefficents(coeff_labels, pca):
+    pc1 = pca.components_[0]
+    pc2 = pca.components_[1]
+    print('Attribute, PC1, PC2:')
+    coeffs = sorted([((pc1_val, pc2_val), label) for label, pc1_val, pc2_val in zip(coeff_labels, pc1, pc2)],
+                    reverse=True)
+    for data in coeffs:
+        print(str(data[1]).ljust(70) + ':' + str(repr(data[0][0])).rjust(25) + str(repr(data[0][1])).rjust(25))
+
+
+def scatter_points(assigned_cluster_numbers, point_labels, principal_components):
+    x = np.transpose(principal_components)[0]
+    y = np.transpose(principal_components)[1]
+    plt.scatter(x, y, c=assigned_cluster_numbers)
+    for i, text in enumerate(point_labels):
+        plt.annotate(text, (x[i], y[i]), ha="center", size=6)
+
+
+def plot_coeffients(coeff_labels, features_count, pca):
+    coeff = np.transpose(pca.components_[0:features_count, :])
+    # since each feature has values for each date:
+    single_feature_values_count = len(coeff_labels)
+    # arrows are scaled so that they are visible on a full graph
+    coeff_scaling_factor = 40
+    cmap = plt.cm.get_cmap(name='jet')
+    colors_norm = colors.Normalize(vmin=0, vmax=features_count)
+    scalar_map = cm.ScalarMappable(norm=colors_norm, cmap=cmap)
+    for i in range(coeff.shape[0]):
+        plt.arrow(0, 0, coeff[i, 0] * coeff_scaling_factor, coeff[i, 1] * coeff_scaling_factor,
+                  color=scalar_map.to_rgba(i // single_feature_values_count), alpha=0.5)
+        plt.text(coeff[i, 0] * coeff_scaling_factor, coeff[i, 1] * coeff_scaling_factor,
+                 coeff_labels[i % len(coeff_labels)], color='b', ha='center', va='center', size=5)
--- show_example_demography_data.py
+++ show_example_demography_data.py
@@ -1,65 +1,70 @@
-from data.WorldBankDataLoader import WorldBankDataLoader
-import pprint
-import pandas
 import os
-from sklearn import cluster, impute, decomposition, svm, preprocessing
-from matplotlib import pyplot as plt
-import numpy as np
+import pprint
 
-# get countries in region - here it's Europe & Central Asia
-all_countries = WorldBankDataLoader().all_countries()
-europe_and_central_asia_countries = {country['name']:None for country in all_countries if country['region']['id'] == 'ECS'}
-for country_name in europe_and_central_asia_countries:
-    country_data_path = os.path.join("demography", "downloaded_countries", country_name + ".csv")
-    europe_and_central_asia_countries[country_name] = pandas.read_csv(country_data_path)
+import pandas
+from sklearn import cluster, impute, preprocessing
+import itertools
+from data.WorldBankDataLoader import WorldBankDataLoader
+from plot_clusters import plot_with_pca
 
-# this example clusters by population density & urban population in time
-# missing values are replaced by mean values of a particular country's data
-NULL_PLACEHOLDER = -1
-imputer = impute.SimpleImputer(missing_values=NULL_PLACEHOLDER, strategy='median')
-used_indicators = ['Population density (people per sq. km of land area)','Urban population (% of total population)']
-data_per_country = {}
-for country_name in europe_and_central_asia_countries:
-    data = europe_and_central_asia_countries[country_name][
-            [used_indicators[0],used_indicators[1]]].fillna(NULL_PLACEHOLDER)
-    data_per_country[country_name] = imputer.fit_transform(data)
+
+def get_data_for_region_per_country(region_name):
+    global europe_and_central_asia_countries
+    all_countries = WorldBankDataLoader().all_countries()
+    selected_countries = {country['name']: None for country in all_countries if country['region']['id'] == region_name}
+    for country_name in selected_countries:
+        country_data_path = os.path.join("demography", "downloaded_countries", country_name + ".csv")
+        selected_countries[country_name] = pandas.read_csv(country_data_path)
+    return selected_countries
+
+
+def replace_nulls(country_data):
+    NULL_PLACEHOLDER = -1
+    imputer = impute.SimpleImputer(missing_values=NULL_PLACEHOLDER, strategy='median')
+    data_per_country = {}
+    for country_name in country_data:
+        data = country_data[country_name][[*used_indicators]].fillna(NULL_PLACEHOLDER)
+        data_per_country[country_name] = imputer.fit_transform(data)
+    return data_per_country
+
+
+europe_and_central_asia_countries = get_data_for_region_per_country('ECS')
+dates = europe_and_central_asia_countries['Poland']['date'].to_numpy()  # any country would suffice, we just need dates
+used_indicators = ['Population density (people per sq. km of land area)',
+                   'Urban population (% of total population)',
+                   "Birth rate, crude (per 1,000 people)",
+                   "Death rate, crude (per 1,000 people)",
+                   "Population, male (% of total population)",
+                   "Sex ratio at birth (male births per female births)",
+                   "Age dependency ratio (% of working-age population)",
+                   "Age dependency ratio, old (% of working-age population)",
+                   "Age dependency ratio, young (% of working-age population)",
+                   "Mortality rate, under-5 (per 1,000 live births)",
+                   "Fertility rate, total (births per woman)"]
+
+data_per_country_without_nulls = replace_nulls(europe_and_central_asia_countries)
 
 # cluster countries with k-means
 clusterer = cluster.KMeans(n_clusters=8, random_state=42)
-# filter out countries without both of expected features (basically Kosovo) - currently hardcoded
-featured_countries = [country for (country, data) in data_per_country.items() if data.shape==(60,2)]
-# flatten arrays for K-means
-X = [preprocessing.scale(data_per_country[country], axis=0).flatten(order='F') for country in featured_countries]
+# filter out countries without all expected features
+featured_countries = [country for (country, data) in data_per_country_without_nulls.items() if
+                      data.shape == (len(dates), len(used_indicators))]
+# scale feature; flatten arrays for K-means
+X = [preprocessing.scale(data_per_country_without_nulls[country], axis=0).flatten(order='F') for country in
+     featured_countries]
+
 labels = clusterer.fit_predict(X)
-grouped_countries = {label:[] for label in labels}
+grouped_countries = {label: [] for label in labels}
 for i in range(len(featured_countries)):
     grouped_countries[labels[i]].append(featured_countries[i])
 
 # show results of clustering
 pprint.pprint(grouped_countries)
 
+feature_names = [indicator + "(" + str(year) + ")" for (indicator, year) in itertools.product(used_indicators, dates)]
+
 # display results of clustering by bringing data into 2 dimensions via PCA
-pca = decomposition.PCA(n_components=2)
-principal_components=pca.fit_transform(X)
-x = np.transpose(principal_components)[0]
-y = np.transpose(principal_components)[1]
-plt.scatter(x, y, c=labels)
-
-coeff = np.transpose(pca.components_[0:2, :])
-
-
-coeff_labels = europe_and_central_asia_countries['Poland']['date'].to_numpy()
-
-n = coeff.shape[0]
-for i in range(n):
-        plt.arrow(0, 0, coeff[i,0]*40, coeff[i,1]*40,color ='r' if (i<60) else 'g',alpha = 0.5)
-        plt.text(coeff[i,0]*40, coeff[i,1]*40, coeff_labels[i % len(coeff_labels)], color = 'b', ha = 'center', va = 'center', size=5)
-
-plot_labels = featured_countries
-for i, text in enumerate(plot_labels):
-    plt.annotate(text, (x[i], y[i]), ha="center", size=6)
-
-plt.show()
+plot_with_pca(X, labels, featured_countries, len(used_indicators), feature_names)
 
 # Areas to reconsider:
 # - how to deal with NaNs - currently mean is used, perhaps we should consider median or a different idea altogether
