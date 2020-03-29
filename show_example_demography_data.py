206e958bc3001e1e143e6e8cb7c297867374bcdc
Aleksandra Drewnik <aleksandra.drewnik@gmail.com>
2020-03-29 13:13:11 +0200

    Added better graphs and data scaling

--- show_example_demography_data.py
+++ show_example_demography_data.py
@@ -2,7 +2,7 @@ from data.WorldBankDataLoader import WorldBankDataLoader
 import pprint
 import pandas
 import os
-from sklearn import cluster, impute, decomposition
+from sklearn import cluster, impute, decomposition, svm, preprocessing
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -16,19 +16,20 @@ for country_name in europe_and_central_asia_countries:
 # this example clusters by population density & urban population in time
 # missing values are replaced by mean values of a particular country's data
 NULL_PLACEHOLDER = -1
-imputer = impute.SimpleImputer(missing_values=NULL_PLACEHOLDER, strategy='mean')
+imputer = impute.SimpleImputer(missing_values=NULL_PLACEHOLDER, strategy='median')
+used_indicators = ['Population density (people per sq. km of land area)','Urban population (% of total population)']
 data_per_country = {}
 for country_name in europe_and_central_asia_countries:
     data = europe_and_central_asia_countries[country_name][
-            ['Population density (people per sq. km of land area)','Urban population (% of total population)']].fillna(NULL_PLACEHOLDER)
+            [used_indicators[0],used_indicators[1]]].fillna(NULL_PLACEHOLDER)
     data_per_country[country_name] = imputer.fit_transform(data)
 
 # cluster countries with k-means
-clusterer = cluster.KMeans(n_clusters=6, random_state=42)
+clusterer = cluster.KMeans(n_clusters=8, random_state=42)
 # filter out countries without both of expected features (basically Kosovo) - currently hardcoded
 featured_countries = [country for (country, data) in data_per_country.items() if data.shape==(60,2)]
 # flatten arrays for K-means
-X = [data_per_country[country].flatten() for country in featured_countries]
+X = [preprocessing.scale(data_per_country[country], axis=0).flatten(order='F') for country in featured_countries]
 labels = clusterer.fit_predict(X)
 grouped_countries = {label:[] for label in labels}
 for i in range(len(featured_countries)):
@@ -43,6 +44,21 @@ principal_components=pca.fit_transform(X)
 x = np.transpose(principal_components)[0]
 y = np.transpose(principal_components)[1]
 plt.scatter(x, y, c=labels)
+
+coeff = np.transpose(pca.components_[0:2, :])
+
+
+coeff_labels = europe_and_central_asia_countries['Poland']['date'].to_numpy()
+
+n = coeff.shape[0]
+for i in range(n):
+        plt.arrow(0, 0, coeff[i,0]*40, coeff[i,1]*40,color ='r' if (i<60) else 'g',alpha = 0.5)
+        plt.text(coeff[i,0]*40, coeff[i,1]*40, coeff_labels[i % len(coeff_labels)], color = 'b', ha = 'center', va = 'center', size=5)
+
+plot_labels = featured_countries
+for i, text in enumerate(plot_labels):
+    plt.annotate(text, (x[i], y[i]), ha="center", size=6)
+
 plt.show()
 
 # Areas to reconsider:
